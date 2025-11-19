f01d5f9e19555947603fcb5d12410c8621a903a9
matheusouzagoncalves <mateusgoncalves635@gmail.com>
2025-11-18 21:34:36 -0300

    Criaçãom da Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,46 @@
 import { Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { ContasComponent } from './pages/contas/contas.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { FornecedoresComponent } from './pages/fornecedores/fornecedores.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RelatoriosComponent } from './pages/relatorios/relatorios.component';
 import { SignUpComponent } from './pages/signup/signup.component';
-import { UserComponent } from './pages/user/user.component';
+import { TransacoesComponent } from './pages/transacoes/transacoes.component';
+
 import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+    { path: 'login', component: LoginComponent },
+
+        {
         path: "signup",
         component: SignUpComponent
         },
+
     {
-        path: "user",
-        component: UserComponent,
-        canActivate: [AuthGuard]
-    }
+    path: 'home',
+    component: HomeComponent,// O componente com o menu e <router-outlet>
+    canActivate: [AuthGuard],
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
+      
+      // Funcionalidades
+      { path: 'dashboard', component: DashboardComponent }, 
+      { path: 'clientes', component: ClientesComponent },
+      { path: 'fornecedores', component: FornecedoresComponent },
+      { path: 'contas', component: ContasComponent },
+      { path: 'transacoes', component: TransacoesComponent },
+      { path: 'relatorios', component: RelatoriosComponent },
+    ],
+  },
+
+  // 3. Rota de Wildcard (página 404/redirecionamento)
+  { path: '**', redirectTo: 'login' },
+
+
+
+
 ];
